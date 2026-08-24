fa04595c787bff9bd670f0b71f5819a888e17977
Ykmmz <yangmengze608@gmail.com>
2026-08-24 18:29:01 +1000

    feat: configure GitHub data backend

---
+++ config.js
@@ -0,0 +1,10 @@
+window.POSITION_LEDGER_CONFIG = {
+  github: {
+    enabled: true,
+    owner: "yangmengze608-afk",
+    repo: "position-ledger",
+    branch: "main",
+    dataDir: "data"
+  },
+  fallbackToLocal: true
+};
